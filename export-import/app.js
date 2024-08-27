0de95aa4a387ded028b07babc2d7cfcf5c46de40
bhadreshce <bhadreshce17@gmail.com>
2024-08-27 18:11:45 +0530

    add

---
+++ export-import/app.js
@@ -0,0 +1,28 @@
+
+
+// const sum = (a, b) => {
+//     console.log(a + b);
+// }
+
+// const multiply = (a, b) => {
+//     console.log(a * b);
+// }
+
+// class Person {
+//     name = 'John';
+//     age = 30;
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+
+//     }
+//     createPerson() {
+//         console.log('person created NAME: ', this.name + 'AGE: ', this.age);
+//     }
+// }
+
+// module.exports = {
+//     sum,
+//     multiply,
+//     Person
+// }
