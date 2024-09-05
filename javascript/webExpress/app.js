8f0a12eb06a423a7a334882ad4201dd24e29481d
bhadreshce <bhadreshce17@gmail.com>
2024-09-05 16:36:45 +0530

    aas

--- javascript/webExpress/app.js
+++ javascript/webExpress/app.js
@@ -3,21 +3,26 @@ const path = require('path');
 const app = express();
 const port = 3000;
 const homePath = path.join(__dirname, 'index.html');
+
 const contactPath = path.join(__dirname, 'contact.html')
 
 console.log(homePath);
 
 app.get('/', (req, res) => {
-    res.sendFile(homePath);
+
+    res.sendFile(homePath)
 })
 
 app.get('/contact', (req, res) => {
+    res.sendFile(contactPath)
 
-    res.sendFile(contactPath);
 })
+
+
 
 
 
 app.listen(port, () => {
-    console.log('listening on port');
+    console.log(`Server running at http://localhost:${port}`)
+    // logs a message when the server is running
 })
--- javascript/webExpress2/app.js
+++ javascript/webExpress2/app.js
@@ -7,10 +7,14 @@ const viewPath = path.join(__dirname, './views')
 const public = path.join(__dirname, './public')
 const partial = path.join(__dirname, './partials')
 
+// settings 
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
 app.use(express.static(public))
 hbs.registerPartials(partial)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 
 // app.set("view engine","hbs")
@@ -27,13 +31,26 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/contact', () => (req, res) => {
+app.get('/contact', (req, res) => {
     res.render('contact', {
         title: 'Home Page',
         name: 'John Doe',
         items: ['Item 1', 'Item 2', 'Item 3']
     })
 })
+
+app.get('/comman', (req, res) => {
+    res.render('pages/comman', {
+        title: 'Home Page',
+        name: 'John Doe',
+        items: ['Item 1', 'Item 2', 'Item 3']
+    })
+})
+
+app.post('/submit', (req, res) => {
+    console.log(req.body);
+})
+
 
 app.listen(port, () => {
     console.log('listening on port');
